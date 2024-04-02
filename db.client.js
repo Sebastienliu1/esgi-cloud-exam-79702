01ebc2702f2ee42e2de1eec306c1982f1d9f6989
Sebastienliu1 <sebastienliu1105@gmail.com>
2024-04-02 12:21:47 +0200

    add db

--- db.client.js
+++ db.client.js
@@ -2,8 +2,12 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  'postgres://fakeurl', // TODO
+  process.env.bn_name, // Database name
+  process.env.bn_user, // User
+  process.env.bn_psd, // Password
   {
+    host: process.env.bn_host, // Host
+    dialect: 'postgres',
     dialectOptions: {
       ssl: {
         require: true,
